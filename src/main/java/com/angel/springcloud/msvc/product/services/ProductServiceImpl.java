b7687af37decbee0d9c2675a8bd2a3289c6e9649
Angel <angelin09ozoz@gmail.com>
2025-01-03 20:48:20 -0600

    Agregando Crud completo para el microservicio msvc-products

--- src/main/java/com/angel/springcloud/msvc/product/services/ProductServiceImpl.java
+++ src/main/java/com/angel/springcloud/msvc/product/services/ProductServiceImpl.java
@@ -39,4 +39,16 @@ public class ProductServiceImpl implements ProductService {
         });
     }
 
+    @Override
+    @Transactional
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
 }
